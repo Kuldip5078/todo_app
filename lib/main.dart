34f7c85ea6f847d790c93146968e954954cf8064
Kuldip5078 <bhuvakuldip34@gmail.com>
2022-08-05 12:58:02 +0530

    file uptedet

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:todo_app/views/task_list.dart';
 
 import 'views/task_details_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
--- lib/views/task_list.dart
+++ lib/views/task_list.dart
@@ -1,23 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:todo_app/controllers/task_list_controller.dart';
 
 class TaskList extends StatelessWidget {
   TaskList({Key? key}) : super(key: key);
-
   final TaskListController _con = Get.put(TaskListController());
+  final Stream<QuerySnapshot> _tasks =
+      FirebaseFirestore.instance.collection('tasks').snapshots();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Obx(() => ListView.builder(
-            itemCount: _con.items.length,
-            itemBuilder: (context, index) => ListTile(
-              title: Text(_con.items[index]),
-              onTap: () => _con.removeItem(index),
-            ),
-          )),
+      body: StreamBuilder(
+        stream: _tasks,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return const Text("Something went wrong");
+          }
+
+          return ListView(
+            children: snapshot.data!.docs.map((DocumentSnapshot doc) {
+              Map<String, dynamic> data = doc.data()! as Map<String, dynamic>;
+              return ListTile(
+                title: Text(data['taskTitle']),
+                subtitle: Text(data['taskDesc']),
+              );
+            }).toList(),
+          );
+        },
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => _con.addItem(),
         child: const Icon(Icons.add),
